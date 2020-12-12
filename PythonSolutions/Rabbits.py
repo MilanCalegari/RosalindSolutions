d91958358fb6802dabc41007417df71959472284
MilanCalegari <mila.calegari@unesp.br>
2020-12-12 20:49:21 -0300

    Recusion_fast() fixed

--- PythonSolutions/Rabbits.py
+++ PythonSolutions/Rabbits.py
@@ -9,15 +9,17 @@ def recursion_naive(n, k):
     else:
         return recursion_naive(n-1,k) + (int(recursion_naive(n-2,k))*k)
 
-def recursion_fast(n, k, computed={0:1, 1:1}):
+def recursion_fast(n, k, computed={1:1, 2:1}):
         if n not in computed:
-            computed[n] = recursion_fast(n-1, k) + (int(recursion_fast(n-2, k)) *k)
+            computed[n] = recursion_fast(n-1, k) + (int(recursion_fast(n-2, k))*k)
             return computed[n]
-        return computed[n]
+        else:
+            return computed[n]
 
 def main():
     n,k = input().split()
-    print(recursion_naive(int(n),int(k)))
+    #print(recursion_naive(int(n),int(k)))
+    print(recursion_fast(int(n), int(k)))
 
 if __name__ == '__main__':
     main()
