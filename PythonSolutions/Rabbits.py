0b97db353fe6e37a7f1ec6e6df9a1f7e117eb616
Rodrigo Milan Calegari <52531634+MilanCalegari@users.noreply.github.com>
2021-04-04 00:05:13 -0300

    time optimization

--- PythonSolutions/Rabbits.py
+++ PythonSolutions/Rabbits.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-
+from fuctions import lru_cache
+## optimization time
 
 #The calculation of number of rabbits is equals Fn = Fn-1 + (Fn-2 * k)
 
-def recursion_naive(n, k):
-    if n <= 1:
-        return n
-    else:
-        return recursion_naive(n-1,k) + (int(recursion_naive(n-2,k))*k)
+# def recursion_naive(n, k):
+#     if n <= 1:
+#         return n
+#     else:
+#         return recursion_naive(n-1,k) + (int(recursion_naive(n-2,k))*k)
 
-def recursion_fast(n, k, computed={1:1, 2:1}):
-        if n not in computed:
-            computed[n] = recursion_fast(n-1, k) + (int(recursion_fast(n-2, k))*k)
-            return computed[n]
-        else:
-            return computed[n]
+# def recursion_fast(n, k, computed={1:1, 2:1}):
+#         if n not in computed:
+#             computed[n] = recursion_fast(n-1, k) + (int(recursion_fast(n-2, k))*k)
+#             return computed[n]
+#         else:
+#             return computed[n]
+
+
+def fib(n, k):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n-1, m) + fib(n-2, m)*m
+
 
 def main():
     n,k = input().split()
